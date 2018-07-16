7f26d83e7c2d73e2d9dfeaaf13ab51ff84b1e222
bryanchun <chun.hiu.sang@gmail.com>
2018-07-16 17:26:14 +0800

    Script for setup

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get install python-pip python-dev build-essential
+sudo pip install --upgrade pip
+python -m pip install --user numpy tensorflow matplotlib scipy scikit-image
+sudo apt-get install python-tk
+
